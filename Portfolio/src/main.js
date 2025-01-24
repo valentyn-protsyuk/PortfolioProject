b6de5a373efb103fb01b2d1ba24bc18d747742d2
Valentyn Protsyuk <valentyn.protsyuk@yahoo.com>
2025-01-24 13:17:41 -0800

    profile section minimum

--- Portfolio/src/main.js
+++ Portfolio/src/main.js
@@ -1,4 +1,5 @@
 import '../styles/modern-normalize.css'
 import '../styles/style.css'
 import '../styles/components/header.css'
+import '../styles/components/profile.css'
 import '../styles/utils.css'
