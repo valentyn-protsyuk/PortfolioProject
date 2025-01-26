1caab40aacda0829d5d8450e670f5ef196d8ff59
Valentyn Protsyuk <valentyn.protsyuk@yahoo.com>
2025-01-25 22:16:14 -0800

    advanced  dark/light mode complete

--- Portfolio/src/utils/theme.js
+++ Portfolio/src/utils/theme.js
@@ -1,11 +1,28 @@
 const theme = () => {
     const themeToggleBtns = document.querySelectorAll('#theme-toggle');
     
-    themeToggleBtns.forEach(btn => {
-        btn.addEventListener('click', () => {
-            document.body.classList.toggle('light-mode');
-        });
-    });
-}
+    //state
+    const curTheme = localStorage.getItem('theme');
+
+    //on load
+    curTheme && document.body.classList.add(curTheme);
+
+    //handler
+    const handleThemeToggle = () => {
+        document.body.classList.toggle('light-mode');
+        //memorize theme
+        if(document.body.classList.contains('light-mode')){
+            localStorage.setItem('theme', 'light-mode');
+        }
+        else{
+            localStorage.removeItem('theme');
+            document.body.removeAttribute('class');
+        }
+    };
+    //toggle theme
+    themeToggleBtns.forEach(btn =>
+        btn.addEventListener('click', handleThemeToggle)
+    );
+};
 
 export default theme;
