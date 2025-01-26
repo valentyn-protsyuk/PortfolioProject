1c6762b95f84ab095d5c76cf8dc8d96df71f7fc3
Valentyn Protsyuk <valentyn.protsyuk@yahoo.com>
2025-01-25 21:39:08 -0800

    mobile navigation complete

--- Portfolio/src/main.js
+++ Portfolio/src/main.js
@@ -1,8 +1,13 @@
-import '../styles/modern-normalize.css'
-import '../styles/style.css'
-import '../styles/components/header.css'
-import '../styles/components/profile.css'
-import '../styles/components/skills.css'
-import '../styles/components/projects.css'
-import '../styles/components/contact.css'
-import '../styles/utils.css'
+import '../styles/modern-normalize.css';
+import '../styles/style.css';
+import '../styles/components/header.css';
+import '../styles/components/profile.css';
+import '../styles/components/skills.css';
+import '../styles/components/projects.css';
+import '../styles/components/contact.css';
+import '../styles/components/footer.css';
+import '../styles/components/mobile-nav.css';
+import '../styles/utils.css';
+import mobileNav from './utils/mobile-nav';
+
+mobileNav();
---
+++ Portfolio/src/utils/mobile-nav.js
@@ -0,0 +1,32 @@
+const mobileNav = () =>{
+    const headerBtn = document.querySelector('.header__bars');
+    const mobileNav = document.querySelector('.mobile-nav');
+    const mobileLinks = document.querySelectorAll('.mobile-nav__link');
+    
+    //state
+    let isMobileNavOpen = false;
+
+    //hamburger btn
+    headerBtn.addEventListener('click', () => {
+        isMobileNavOpen = !isMobileNavOpen;
+        if(isMobileNavOpen){
+            mobileNav.style.display = 'flex';
+            document.body.style.overflowY = 'hidden';
+        }else{
+            mobileNav.style.display = 'none';
+            document.body.style.overflowY = 'auto';
+        }
+    });
+
+    //menu links
+    mobileLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            isMobileNavOpen = false;
+            mobileNav.style.display = 'none';
+            document.body.style.overflowY = 'auto';
+        })
+    });
+};
+
+export default mobileNav;
+
